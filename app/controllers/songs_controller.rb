ccefb6ce0874e334dd848ac7c2c1567655b7b4ad
Grace Choi <oiograceoio@gmail.com>
2019-04-05 20:01:24 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,17 +9,17 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
 
+  post '/songs' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    redirect to "/songs/#{@song.slug}"
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
-  end
-
-  post '/songs' do
-    @song = Song.create(name: params["Name"])
-    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
-    @song.genre_id = params[:genres]
-    @song.save
-    redirect to "/songs/#{@song.slug}"
   end
 
   get '/songs/:slug/edit' do
@@ -30,8 +30,8 @@ class SongsController < ApplicationController
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     @song.update(params[:song])
-    @song.artist = Artist.find_or_create_by(name: params["artist"]["name"])
-    @song.genre_id = params[:genres]
+    @song.artist = Artist.find_or_create_by(:name => params["artist"]["name"])
+    @song.genre_ids = params[:genres]
     @song.save
     redirect to "/songs/#{@song.slug}"
   end
